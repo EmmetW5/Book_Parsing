20f25b013fb39d8b66ee4a6e2102b9092a9ecaf0
Emmet Whitehead <whitee6@rpi.edu>
2024-08-01 12:20:08 -0400

    Added More text files, running parser

--- ai_text_parse/ExcelParser.py
+++ ai_text_parse/ExcelParser.py
@@ -170,6 +170,13 @@ class ExcelParser:
             # <Notes> : len = 7
             elif(self.compare_str_fuzz("<Notes>", lines[i][0:7])):
                 current_company.add_notes(self.remove_brackets(lines[i]))
+
+            else:
+                # If we don't recognize the line, write the line to the error.txt file
+                # Company name: error
+                with open('error.txt', 'a') as file:
+                    file.write(f"Company name: {current_company.name}\n")
+                    file.write(f"Error: {lines[i]}\n\n")
             i += 1
         return companies
 
